f1c41164b18b4324192186c0e396d33ec8a46203
ChaDown <charliedownes@hotmail.com>
2024-07-12 17:09:14 +0100

    Added styling and How it works component

--- src/app/how-it-works/how-it-works.component.ts
+++ src/app/how-it-works/how-it-works.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-how-it-works',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RouterLink],
   templateUrl: './how-it-works.component.html',
   styleUrl: './how-it-works.component.scss'
 })
